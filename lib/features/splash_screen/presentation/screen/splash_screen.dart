8cd46e7807ab6ab60572d286cee50b4e1a2421ab
Yeasin00p <07yeasin1981@gmail.com>
2025-07-14 20:15:49 +0600

    splash screen

---
+++ lib/features/splash_screen/presentation/screen/splash_screen.dart
@@ -0,0 +1,16 @@
+import 'package:expense_tracker_app/core/widget/logo_widget.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(backgroundColor: Color(0xFFFEF7FF), body: LogoWidget());
+  }
+}
